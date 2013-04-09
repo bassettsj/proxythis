2923dafddb2e8d8a8651afa9ce6889cb9b1cc956
Steven Bassett <steven.j.bassett@gmail.com>
2013-04-09 10:45:39 -0400

    Adding a copy to clipboard function to the module.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ proxythis
 =========
 
 Drupal module to create a block that you can configure to modify a link to use a proxy for resources that are not available off campus.
+
+
+
+**Must specify path for init.js to load the swf ** 
---
+++ js/init.js
@@ -0,0 +1,13 @@
+(function ($) {
+  $(document).ready(function () {
+    //load zeroclipboard
+    var clip = new ZeroClipboard($('#copy-dynamic'), {
+      moviePath: '/sites/all/modules/_custom/proxythis/js/ZeroClipboard.swf',
+    });
+
+  });
+})(jQuery);
+
+
+
+
