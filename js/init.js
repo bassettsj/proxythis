cde7545002e6c7851ba65a2c474e223f8ffa207d
Steven Bassett <steven.j.bassett@gmail.com>
2013-05-01 12:06:46 -0400

    Adding a block to the module

--- js/init.js
+++ js/init.js
@@ -2,7 +2,7 @@
   $(document).ready(function () {
     //load zeroclipboard
     var clip = new ZeroClipboard($('#copy-dynamic'), {
-      moviePath: '/sites/all/modules/_custom/proxythis/js/ZeroClipboard.swf',
+      moviePath: '/sites/all/modules/custom/proxythis/js/ZeroClipboard.swf',
     });
 
   });
