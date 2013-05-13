852f553732f63da04151e9abf23e04438adcbf3f
Steven Bassett <steven.j.bassett@gmail.com>
2013-05-13 12:34:44 -0400

    Adding the libraries api to load the javascript and the swf for the clipboard functions to load

--- js/init.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-  $(document).ready(function () {
-    //load zeroclipboard
-    var clip = new ZeroClipboard($('#copy-dynamic'), {
-      moviePath: '/sites/all/modules/custom/proxythis/js/ZeroClipboard.swf',
-    });
-
-  });
-})(jQuery);
